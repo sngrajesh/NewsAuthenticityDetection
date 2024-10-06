d7c1ef19f77e272b8e23bff5daf661cbace08083
Rajesh Singh <rajeshnksingh@gmail.com>
2024-10-06 23:35:05 +0530

    Flask application created

---
+++ app.py
@@ -0,0 +1,40 @@
+import re
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('stopwords', quiet=True)
+
+app = Flask(__name__)
+
+# Load the model and vectorizer
+with open('models/model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('models/vectorizer.pkl', 'rb') as vectorizer_file:
+    vectorizer = pickle.load(vectorizer_file)
+
+def preprocess_text(text):
+    # Remove punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+    # Convert to lowercase and remove stopwords
+    words = text.lower().split()
+    words = [word for word in words if word not in stopwords.words('english')]
+    return ' '.join(words)
+
+@app.route('/', methods=['GET', 'POST'])
+def home(): 
+    prediction = None
+    if request.method == 'POST':
+        if request.form['news_text'] == '':
+            return render_template('index.html', prediction=prediction)
+        news_text = request.form['news_text']
+        preprocessed_text = preprocess_text(news_text)
+        vectorized_text = vectorizer.transform([preprocessed_text])
+        prediction = model.predict(vectorized_text)[0]
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
